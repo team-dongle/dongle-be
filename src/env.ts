87a92c173673ea82c10b8226f7ad7884e8c62813
조용원 <auau3@kakao.com>
2024-09-27 13:31:53 +0900

    fix: mysql db 환경변수 추가 - mysql 접속을 위한 connection 정보 환경 변수 추가

--- src/env.ts
+++ src/env.ts
@@ -9,4 +9,11 @@ dotenv.config({
 export const env = {
     port: process.env.APP_PORT,
     origins: process.env.APP_ORIGINS.split(",") || [],
+    db: {
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
+        username: process.env.MYSQL_USERNAME,
+        password: process.env.MYSQL_PASSWORD,
+        schema: process.env.MYSQL_DATABASE,
+    },
 };
